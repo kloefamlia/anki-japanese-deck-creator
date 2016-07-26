59de3a180b2f88117d1dfcc241c0971aec17bcab
nfarrantdiaz <nathan@ntree.com>
2016-07-26 13:33:57 -0400

    dependencies

--- parse-anki.sh
+++ parse-anki.sh
@@ -3,6 +3,8 @@
 # this script takes in a japanese word as input and uses the jisho.org api to parse it into a csv file readable by anki
 # note that the parse cards are parsed into a format which matches syntax that I have personally created for myself for anki cards
 # if you don't like the parsed format or cant figure it out you should change this script
+#
+# script is dependent on jq to parse data
 inputword="$1"
 wordjson=$(curl -XGET http://jisho.org/api/v1/search/words?keyword="${1}")
 
