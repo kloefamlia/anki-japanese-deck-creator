8cd55aaded22e465b7049b82a72a93ed40056f23
nfarrantdiaz <nathan@ntree.com>
2016-07-26 13:33:08 -0400

    initial commit

---
+++ parse-anki.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# this script takes in a japanese word as input and uses the jisho.org api to parse it into a csv file readable by anki
+# note that the parse cards are parsed into a format which matches syntax that I have personally created for myself for anki cards
+# if you don't like the parsed format or cant figure it out you should change this script
+inputword="$1"
+wordjson=$(curl -XGET http://jisho.org/api/v1/search/words?keyword="${1}")
+
+
+echo ${wordjson}
+
+Front="${inputword}"
+Back=""
+Example=""
+Reading=$(echo ${wordjson} | jq '.data[0].japanese[0].reading')
+Linguistic_function=""
+
+echo "the variables are..."
+echo ${Front}
+echo ${Back}
+echo ${Example}
+echo ${Reading}
+echo ${Linguistic_function}
